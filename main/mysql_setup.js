d0dd27c705e68631826f209a6191f70ab7a0f293
Luis Belloch <luis.bellochmartinez@gmail.com>
2021-09-20 19:20:09 +0200

    database_conexion
    corrigiendo bugs del commit

--- main/mysql_setup.js
+++ main/mysql_setup.js
@@ -1,11 +1,16 @@
 var mysql = require('mysql');
+<<<<<<< Updated upstream
 var pool = mysql.createPool({
+=======
+var conn = mysql.createConnection({
+>>>>>>> Stashed changes
     host : "localhost",
     user : "root",
     password : "",
     database : "biometric_database"
 });
 
+<<<<<<< Updated upstream
 pool.getConnection(function(error, result){
     if(error) {
         throw error;
@@ -15,6 +20,8 @@ pool.getConnection(function(error, result){
     }
 });
 
+=======
+>>>>>>> Stashed changes
 var queryString = "SELECT * from Medidas";
 conn.query(queryString, function(error, result){
     if(error) {
