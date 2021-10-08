955a1cb537e5349a8aa76f8c823b568a0a46c1f3
Luis Belloch <luis.bellochmartinez@gmail.com>
2021-10-08 12:59:19 +0200

    get-funciones
    get implementados, diseño modificado

--- servidor/controladores/usuariosControlador.js
+++ servidor/controladores/usuariosControlador.js
@@ -14,13 +14,38 @@ class UsuariosControlador {
     /**
      * Devuelve un JSON con todos los usuarios
      *
-     * @return {text} JSON con los sensores
+     * @return {text} JSON con los usuarios
      */
      static getTodosLosUsuarios() {
         // Recibe las medidas
         return new Promise(result => {
 
             var queryString = "SELECT usuarios.ID, usuarios.Correo, usuarios.Nombre from usuarios";
+
+            pool.getConnection((err, connection) => {
+                if(err) throw err;
+                console.log('connected as id ' + connection.threadId);
+                connection.query(queryString, (err, rows) => 
+                {
+                    connection.release(); // devuelve la conexion al pool
+                    // Si hay un error devuelve el error
+                    if(err) throw err;
+                    result(rows);
+                });
+            }); 
+        });
+    }
+    /**
+     * Devuelve un JSON con un usuario
+     *
+     * @params {number} id del usuario
+     * @return {text} JSON con el usuario
+     */
+     static getUsuario(id) {
+        // Recibe las medidas
+        return new Promise(result => {
+
+            var queryString = 'SELECT * from usuarios WHERE usuarios.ID = "' + id + '"';
 
             pool.getConnection((err, connection) => {
                 if(err) throw err;
